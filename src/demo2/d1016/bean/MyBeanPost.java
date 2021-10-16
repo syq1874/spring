68da5daf949f8a9851bbe43c9ad75102ad955ed9
19537 <1953745128@qq.com>
2021-10-16 21:34:00 +0800

    bean的生命周期2

---
+++ src/demo2/d1016/bean/MyBeanPost.java
@@ -0,0 +1,20 @@
+package demo2.d1016.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyBeanPost implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("在初始化之前执行的方法");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("在初始化之后执行的方法");
+        return bean;
+    }
+
+}
