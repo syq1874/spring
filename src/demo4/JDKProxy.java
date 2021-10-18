0eca6880aac8f5f2752b9d51fbaf0fced288b95e
19537 <1953745128@qq.com>
2021-10-18 20:26:16 +0800

    JDK动态代理

---
+++ src/demo4/JDKProxy.java
@@ -0,0 +1,35 @@
+package demo4;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
+public class JDKProxy {
+
+    public static void main(String[] args) {
+        Class[] interfaces = {UserDao.class};
+        UserDaoImpl userDao =new UserDaoImpl();
+        UserDao dao = (UserDao) Proxy.newProxyInstance(JDKProxy.class.getClassLoader(), interfaces,new UserDaoProxy(userDao));
+        int result = dao.add(1, 2);
+        System.out.println(result);
+    }
+}
+
+class UserDaoProxy implements InvocationHandler{
+
+    private Object obj;
+    public UserDaoProxy(Object obj){
+        this.obj=obj;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("方法之前执行..."+method.getName()+":传递的参数..."+ Arrays.toString(args));
+
+        Object res = method.invoke(obj, args);
+
+        System.out.println("方法之后执行..."+obj);
+        return res;
+    }
+}
---
+++ src/demo4/UserDao.java
@@ -0,0 +1,8 @@
+package demo4;
+
+public interface UserDao {
+
+    public int add(int a,int b);
+
+    public String update(String id);
+}
