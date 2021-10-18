b757c1205c66a0bb68e10e13d6c37fb6b3dd0a3b
19537 <1953745128@qq.com>
2021-10-18 18:03:11 +0800

    使用配置类，代替xml配置文件

---
+++ src/demo3/d1018/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package demo3.d1018.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"demo3.d1018"})
+public class SpringConfig {
+}
--- src/demo3/d1018/test/SpringTest.java
+++ src/demo3/d1018/test/SpringTest.java
@@ -1,8 +1,10 @@
 package demo3.d1018.test;
 
+import demo3.d1018.config.SpringConfig;
 import demo3.d1018.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringTest {
@@ -11,6 +13,15 @@ public class SpringTest {
     public void test1(){
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("demo3/d1018/bean1.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        System.out.println(userService);
+        userService.add();
+    }
+
+    @Test
+    public void test2(){
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(SpringConfig.class);
         UserService userService = context.getBean("userService", UserService.class);
         System.out.println(userService);
         userService.add();
