8212ccc7a97f3d69c5688ea132c6bfafb9a7bbc2
19537 <1953745128@qq.com>
2021-10-19 20:04:51 +0800

    Aop_五大增强方式

--- src/aop/demo1/UserProxy.java
+++ src/aop/demo1/UserProxy.java
@@ -1,7 +1,7 @@
 package aop.demo1;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,5 +11,27 @@ public class UserProxy {
     @Before(value = "execution(* aop.demo1.User.add(..))")
     public void before(){
         System.out.println("before");
+    }
+
+    @After(value = "execution(* aop.demo1.User.add(..))")
+    public void after(){
+        System.out.println("after");
+    }
+
+    @AfterReturning(value = "execution(* aop.demo1.User.add(..))")
+    public void afterReturning(){
+        System.out.println("afterReturning");
+    }
+
+    @AfterThrowing(value = "execution(* aop.demo1.User.add(..))")
+    public void afterThrowing(){
+        System.out.println("afterThrowing");
+    }
+
+    @Around(value = "execution(* aop.demo1.User.add(..))")
+    public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("环绕之前");
+        proceedingJoinPoint.proceed();
+        System.out.println("环绕之后");
     }
 }
